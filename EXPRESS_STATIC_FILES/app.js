628b61122dad4ea7446c33d8afa9e3f296895d22
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-05-31 08:50:41 +0000

    progetto finito

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express(); 
 const path = require('path');
-app.get('/home',function(req,res){
- res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartella del progetto
+app.get('/',function(req,res){
+ res.sendFile(path.join(__dirname, 'index.html')); 
 });
 app.get('/about',function(req,res){
- res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del progetto
+ res.sendFile(path.join(__dirname,'about.html')); 
 });
 app.get('/sitemap',function(req,res){
- res.sendFile(path.join(__dirname,'sitemap.html')); //__dirname : Ritorna la cartella del progetto
+ res.sendFile(path.join(__dirname,'sitemap.html')); 
+});
+app.get('/lista',function(req,res){
+ res.sendFile(path.join(__dirname,'comingsoon.json')); 
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
